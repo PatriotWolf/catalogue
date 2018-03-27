e1b418ef72a0ab742218b8ef6192742ec1ce5234
syah <syahfisal37@yahoo.com>
2018-03-27 14:40:14 +0800

    use bootswatch template, add navbar, footer, routing, ngbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-
+import { AppRoutingModule } from './/app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule,NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import {GetlistService} from '../services/getlist.service';
+import { Router } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+	images: Array<string>;;
 
   constructor() { }
 
   ngOnInit() {
+  	this.images=["http://crizaze.com/wp-content/uploads/2014/12/modeling-agency.jpg","https://i.ytimg.com/vi/sDf0LKR82-4/maxresdefault.jpg","http://jonvilma.com/images/model-18.jpg"]
   }
 
 }
